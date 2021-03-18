11345d3016377c2097bf4ea1d0549ca3633a8199
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-03-18 20:48:27 +0900

    削除機能（修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :unless, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update,:destroy]
+  before_action :unless, only: [:edit, :update,:destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -34,8 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
