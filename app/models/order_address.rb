1a2e2565b2a873c7e3f0234f514d286205ee37bd
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-04-10 14:51:44 +0900

    10桁の電話番号にも対応。

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,12 +2,11 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :post_num, :city, :shipment_source_id, :address, :build_name, :phone_num, :user_id, :item_id, :token
 
-  
   with_options presence: true do
     validates :post_num, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :address
-    validates :phone_num, format: { with: /\A\d{11}\z/, message: 'input only number' }
+    validates :phone_num, format: { with: /\A\d{10,11}\z/, message: 'input only number' }
     validates :token
     validates :user_id
     validates :item_id
