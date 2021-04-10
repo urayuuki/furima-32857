b6020a27a1ce3ef3d25a7b0a7ea8db5a3590d2dd
urayuuki <76430661+urayuuki@users.noreply.github.com>
2021-04-10 14:53:12 +0900

    その他

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     shipment_source_id { 4 }
     city { '豊中' }
     address { '上新 1-2-3' }
-    build_name { 'xxx'}
+    build_name { 'xxx' }
     phone_num { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
   end
